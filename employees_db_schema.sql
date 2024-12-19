28499e28bc38f6741a4eadb15521f3f85e4c2fa7
Saurabh_Lakhanpal <ba.saurabh.lakhanpal@gmail.com>
2024-12-19 15:28:32 -0500

    uploading schema

--- employees_db_schema.sql
+++ employees_db_schema.sql
@@ -1,26 +1,36 @@
--- Drop the tables if they exist
-DROP TABLE IF EXISTS departments;
+-- Drop tables that reference others first
 DROP TABLE IF EXISTS dept_emp;
 DROP TABLE IF EXISTS dept_manager;
-DROP TABLE IF EXISTS employees;
 DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS departments;
 DROP TABLE IF EXISTS titles;
 
+-- Create titles first since it's not dependent on any other table
+CREATE TABLE titles (
+    title_id CHAR(5) PRIMARY KEY,
+    title VARCHAR(20) NOT NULL
+);
+
+-- Now create employees table, referencing titles
+CREATE TABLE employees (
+    emp_no INTEGER PRIMARY KEY,
+    emp_title_id CHAR(5) NOT NULL,
+    birth_date DATE, 
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20),
+    sex CHAR(1) NOT NULL,
+    hire_date DATE NOT NULL,
+    FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+-- Create departments table
 CREATE TABLE departments (
     dept_no CHAR(4) PRIMARY KEY,
     dept_name VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE employees (
-    emp_no INTEGER PRIMARY KEY,
-    emp_title_id CHAR(5) NOT NULL,
-    birth_date DATE NOT NULL,
-    first_name VARCHAR(20) NOT NULL,
-    last_name VARCHAR(20) NOT NULL,
-    sex CHAR(1) NOT NULL,
-    hire_date DATE NOT NULL
-);
-
+-- Create dept_emp table
 CREATE TABLE dept_emp (
     emp_no INTEGER NOT NULL,
     dept_no CHAR(4) NOT NULL,
@@ -29,6 +39,7 @@ CREATE TABLE dept_emp (
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
+-- Create dept_manager table
 CREATE TABLE dept_manager (
     dept_no CHAR(4) NOT NULL,
     emp_no INTEGER NOT NULL,
@@ -37,13 +48,9 @@ CREATE TABLE dept_manager (
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
+-- Create salaries table
 CREATE TABLE salaries (
     emp_no INTEGER NOT NULL PRIMARY KEY,
     salary INTEGER DEFAULT 0 CHECK (salary >= 0),
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
-);
-
-CREATE TABLE titles (
-    title_id CHAR(5) PRIMARY KEY,
-    title VARCHAR(20) NOT NULL
 );
